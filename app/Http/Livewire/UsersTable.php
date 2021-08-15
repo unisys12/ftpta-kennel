3aa4ade19d83f21cb0491e5ead2c88a421e8dc0b
Phillip Jackson <unisys12@gmail.com>
2021-08-15 06:58:55 -0500

    convert users table to powergrid

---
+++ app/Http/Livewire/UsersTable.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\PowerGrid;
+use PowerComponents\LivewirePowerGrid\PowerGridEloquent;
+use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+
+class UsersTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    public function setUp()
+    {
+        $this->showCheckBox()
+            ->showPerPage()
+            ->showExportOption('download', ['excel', 'csv'])
+            ->showSearchInput();
+    }
+
+    public function template(): ?string
+    {
+        return null;
+    }
+
+    public function dataSource(): ?Builder
+    {
+        return User::query();
+    }
+
+    public function addColumns(): ?PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('id')
+            ->addColumn('name')
+            ->addColumn('email')
+            ->addColumn('created_at_formatted', function(User $model) { 
+                return Carbon::parse($model->created_at)->format('d/m/Y H:i:s');
+            })
+            ->addColumn('updated_at_formatted', function(User $model) { 
+                return Carbon::parse($model->updated_at)->format('d/m/Y H:i:s');
+            });
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::add()
+                ->title(__('ID'))
+                ->field('id')
+                ->sortable()
+                ->searchable(),
+
+            Column::add()
+                ->title(__('NAME'))
+                ->field('name')
+                ->sortable()
+                ->searchable(),
+
+            Column::add()
+                ->title(__('EMAIL'))
+                ->field('email')
+                ->sortable()
+                ->searchable(),
+
+            Column::add()
+                ->title(__('CREATED AT'))
+                ->field('created_at_formatted')
+                ->searchable()
+                ->sortable()
+                ->makeInputDatePicker('created_at'),
+
+            Column::add()
+                ->title(__('UPDATED AT'))
+                ->field('updated_at_formatted')
+                ->searchable()
+                ->sortable()
+                ->makeInputDatePicker('updated_at'),
+
+        ]
+;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable this section only when you have defined routes for these actions.
+    |
+    */
+
+    /*
+    public function actions(): array
+    {
+       return [
+           Button::add('edit')
+               ->caption(__('Edit'))
+               ->class('bg-indigo-500 text-white')
+               ->route('user.edit', ['user' => 'id']),
+
+           Button::add('destroy')
+               ->caption(__('Delete'))
+               ->class('bg-red-500 text-white')
+               ->route('user.destroy', ['user' => 'id'])
+               ->method('delete')
+        ];
+    }
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Edit Method
+    |--------------------------------------------------------------------------
+    | Enable this section to use editOnClick() or toggleable() methods
+    |
+    */
+
+    /*
+    public function update(array $data ): bool
+    {
+       try {
+           $updated = User::query()->find($data['id'])->update([
+                $data['field'] => $data['value']
+           ]);
+       } catch (QueryException $exception) {
+           $updated = false;
+       }
+       return $updated;
+    }
+
+    public function updateMessages(string $status, string $field = '_default_message'): string
+    {
+        $updateMessages = [
+            'success'   => [
+                '_default_message' => __('Data has been updated successfully!'),
+                //'custom_field' => __('Custom Field updated successfully!'),
+            ],
+            "error" => [
+                '_default_message' => __('Error updating the data.'),
+                //'custom_field' => __('Error updating custom field.'),
+            ]
+        ];
+
+        return ($updateMessages[$status][$field] ?? $updateMessages[$status]['_default_message']);
+    }
+    */
+}
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,57 +11,7 @@
                     @if (count($users) < 1)
                     <p>There are currently no users!
                     @else
-                    <table>
-                        <thead>
-                            <tr>
-                                <th class="p-1 border border-indigo-400">ID</th>
-                                <th class="p-1 border border-indigo-400">NAME</th>
-                                <th class="p-1 border border-indigo-400">EMAIL</th>
-                                <th class="p-1 border border-indigo-400" colspan="3">ROLES</th>
-                                <th class="p-1 border border-indigo-400">CREATED</th>
-                                <th class="p-1 border border-indigo-400">UPDATED</th>
-                                <th class="p-1 border border-indigo-400" colspan="2">ACTIONS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
-                                <tr class="hover:bg-indigo-50 transition">
-                                    <td class="py-1 px-2 border border-indigo-300">{{ $user->id }}</td>
-                                    <td class="py-1 px-2 border border-indigo-300">
-                                        <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
-                                    </td>
-                                    <td class="py-1 px-2 border border-indigo-300">{{ $user->email }}</td>
-                                    <td class="py-1 px-2 border border-indigo-300" colspan="3">
-                                    @foreach ($roles as $role)
-                                            <label for="{{ $role->name }}">{{ $role->name }}</label>
-                                            <input
-                                            type="checkbox"
-                                            name="roles[]"
-                                            class="mx-2"
-                                            id="{{ $role->name }}_checkbox"
-                                            value="{{ $role->id }}"
-                                            @foreach ($user->roles as $assigned_role)
-                                                @if($assigned_role->pivot->role_id == $role->id) checked @endif
-                                            @endforeach
-                                            >
-                                    @endforeach
-                                    </td>
-                                    <td class="py-1 px-2 border border-indigo-300">{{$user->created_at }}</td>
-                                    <td class="py-1 px-2 border border-indigo-300">{{$user->updated_at }}</td>
-                                    <td class="py-1 px-2 border border-indigo-300">
-                                        <a href="{{ route('users.edit', $user) }}">
-                                            <button class="p-2 rounded bg-green-500 hover:bg-green-600 text-gray-100 hover:text-gray-50 transition">EDIT</button>
-                                        </a>
-                                    </td>
-                                    <td class="py-1 px-2 border border-indigo-300">
-                                        <a href="{{ route('users.destroy', $user) }}">
-                                            <button class="p-2 rounded bg-red-500 hover:bg-red-600 text-gray-100 hover:text-gray-50 transition">DELETE</button>
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <livewire:users-table/>
                     @endif
                 </div>
             </div>
